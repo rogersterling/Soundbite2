79ace91ee2bd93d1476622d2e4adc9bccbbc0425
Sam Gaddis <samgaddis@gmail.com>
2024-08-03 19:54:47 -0500

    Initial Commit

---
+++ Soundbite/SoundbiteApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoundbiteApp.swift
+//  Soundbite
+//
+//  Created by Sam Gaddis on 8/3/24.
+//
+
+import SwiftUI
+
+@main
+struct SoundbiteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
